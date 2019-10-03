b4b9d7819db3f34b444eedc62f68cf01f5f897c6
owndigitaldev <1kopidea>
2019-10-03 14:31:53 +0700

    Initial

---
+++ app/src/main/java/com/androstock/speechtotext/Function.java
@@ -0,0 +1,24 @@
+package com.androstock.speechtotext;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Created by SHAJIB on 8/2/2017.
+ */
+
+public class Function {
+
+    public static  boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
